529966a6d5141e1fd618aac1047a58afcbc46aed
Avdiienko Illia <avil48job@gmail.com>
2023-04-14 15:24:09 +0300

    feat(front-end): make get endpoint return actually something useful

--- src/routes/upload.py
+++ src/routes/upload.py
@@ -1,11 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 upload_bp = Blueprint("upload", __name__)
 
 
 @upload_bp.get("/")
 def show_upload_form():
-    ...
+    return render_template("upload.html")
 
 
 @upload_bp.post("/")
